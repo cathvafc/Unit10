42836d431a0a6d5171fed1bbcf6e0a85642cb80b
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-07 11:40:55 +0100

    Exercise 1

---
+++ Unit10/Unit10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Unit10/Unit10/dto/Cuenta.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit10.dto
+{
+    class Cuenta
+    {
+        private string titular = "";
+        private double cantidad = 0;
+
+        public Cuenta(string titular)
+        {
+            this.titular = titular;        
+        }
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public void Ingresar (double cantidad)
+        {
+
+            if (cantidad > 0 )
+            {
+                this.cantidad += cantidad;
+            }
+
+        }
+
+        public void Retirar(double cantidad)
+        {
+            this.cantidad -= cantidad;
+
+            if (this.cantidad < 0)
+                this.cantidad = 0;
+
+        }
+
+
+        public void setTitular(string titular)
+        {
+            this.titular = titular;
+        }
+
+        public void setCantidad(double cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+
+        public string getTitular ()
+        {
+            return titular;
+        }
+
+        public double getCantidad()
+        {
+            return cantidad;
+        }
+
+        public string toString()
+        {
+            return titular + cantidad;
+        }
+
+    }
+}
