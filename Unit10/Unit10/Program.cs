dc0e7ce05b0318f9ae5993a778ea714e682bd41d
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-08 08:02:40 +0100

    Exercise 2

--- Unit10/Unit10/Program.cs
+++ Unit10/Unit10/Program.cs
@@ -6,7 +6,87 @@ namespace Unit10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int edad = 0;
+            char sexo;
+            double peso = 0;
+            double altura = 0;
+            string name = "";
+
+            
+          
+
+            Console.WriteLine("Inserta el nombre: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Inserta el sexo: H (hombre) o M (Mujel) ");
+            sexo = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Inserta el peso en kg: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Inserta la edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserta la altura en metros: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Persona person1 = new Persona(name,edad,sexo,peso,altura);
+            Persona person2 = new Persona(name,edad,sexo);
+            Persona person3 = new Persona();
+
+            Console.WriteLine("----------------------------------------------------------------");
+
+           person3.setNombre("Jorgito");
+            person3.setEdad(18);
+            person3.setSexo('X');
+            person3.setPeso(75);
+            person3.setAltura(170);
+
+            Console.WriteLine("----------------------------------------------------------------");
+
+            TieneSobrepeso(person1);
+            TieneSobrepeso(person2);
+            TieneSobrepeso(person3);
+
+            Console.WriteLine("----------------------------------------------------------------");
+
+            EsMayorDeEdad(person1);
+            EsMayorDeEdad(person2);
+            EsMayorDeEdad(person3);
+
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.WriteLine("{0}", person1.toString()); 
+            Console.WriteLine("{0}", person2.toString()); 
+            Console.WriteLine("{0}", person3.toString());
+
+            Console.WriteLine("----------------------------------------------------------------");
+
+        }
+
+        public static void TieneSobrepeso(Persona person)
+        {
+            if (person.CalcularIMC() == -1)
+            {
+                Console.WriteLine(" {0} está por debajo de su peso Ideal", person.getName());
+            }
+            else if (person.CalcularIMC() == 0)
+            {
+                Console.WriteLine("{0} está en su peso ideal", person.getName());
+            }
+            else
+            {
+                Console.WriteLine("{0} está con sobrepeso", person.getName());
+            }
+        }
+
+        public static void EsMayorDeEdad(Persona person)
+        {
+            if(person.EsMayorDeEdad())
+            {
+                Console.WriteLine("{0} es mayor de edad", person.getName());
+            }
+            else
+            {
+                Console.WriteLine("{0} no es mayor de edad", person.getName());
+            }
         }
     }
 }
--- Unit10/Unit10/dto/Persona.cs
+++ Unit10/Unit10/dto/Persona.cs
@@ -6,43 +6,143 @@ namespace Unit10
 {
     class Persona
     {
-        const string H = "Hombre";
-        const string M = "Mujer";
+        Random rand = new Random();
+
+        const char H = 'H';
+        const char M = 'M';
+        const int LOWIMC = -1;
+        const int MIDIMC = 0;
+        const int HIGHIMC = 1;
 
         private string name = "";
         private int edad = 0;
         private string dni = "";
-        private string sexo = H;
+        private char sexo = H;
         private double peso = 0;
         private double altura = 0;
         public Persona()
         {
-            
+            GeneraDNI();
         }
-        public Persona(string name, int edad, string sexo)
+        public Persona(string name, int edad, char sexo)
         {
             this.name = name;
             this.edad = edad;
             this.sexo = sexo;
-
+            ComprobarSexo(sexo);
+            GeneraDNI();
         }
 
-        public Persona(string name, int edad, string sexo, double peso, double altura, string dni)
+        public Persona(string name, int edad, char sexo, double peso, double altura)
         {
             this.name = name;
             this.edad = edad;
             this.sexo = sexo;
+            ComprobarSexo(sexo);
             this.peso = peso;
             this.altura = altura;
-            this.dni = dni;
+  
+            GeneraDNI();
         }
+
+        public int CalcularIMC()
+        {
+            double result = peso / Math.Pow(altura, 2);
+
+            if (result < 20)
+            {
+                return LOWIMC;
+            }
+            else if (20 <= result && result <= 25)
+            {
+                return MIDIMC;
+            }
+            else
+            {
+                return HIGHIMC;
+            }
+        }
+
+        public bool EsMayorDeEdad()
+        {
+            if (edad >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
         public void setNombre(string name)
         {
             this.name = name;
         }
-        public void Saludar()
+        public void setEdad (int edad)
         {
-            Console.WriteLine("Hola, soy {0}", name);
+            this.edad = edad;
         }
+
+        public void setSexo(char sexo)
+        {
+            this.sexo = sexo;
+            ComprobarSexo(sexo); //Siempre comprobamos el sexo y si no es H o M, por defecto será H.
+        }
+
+        public void setPeso (double peso)
+        {
+            this.peso = peso;
+        }
+
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+
+        public string toString()
+        {
+            return string.Format("Name: {0}, DNI: {1} Peso: {2} Altura: {3}, Sexo: {4}, Edad: {5}.", name, dni, peso, altura, sexo , edad);
+        }
+
+        public string getName ()
+        {
+            return name;
+        }
+
+
+        /// <summary>
+        /// Se comprueba si el sexo es M o H. Por defecto H.
+        /// </summary>
+        /// <param name="sexo"></param>
+        private void ComprobarSexo(char sexo)
+        {
+            if (sexo != H || sexo != M)
+                this.sexo = H;
+        }
+        private void GeneraDNI()
+        {
+            char letraDNI;
+            string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int longitud = 0;
+           
+            longitud = caracteres.Length;
+            letraDNI = caracteres[rand.Next(longitud)];
+
+
+            int longitudNumDni = 8;
+
+            for (int i = 0; i < longitudNumDni; i++)
+            {
+                dni += rand.Next(0, 9);
+            }
+
+            dni += letraDNI;
+
+        }
+
+        
     }
 }
